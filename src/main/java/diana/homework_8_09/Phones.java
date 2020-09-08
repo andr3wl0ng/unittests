6e59b5c32a30d98b38b9b557b7f84185a98e56b5
dtrehubova <dtrehubova@credible.com>
2020-09-08 11:24:29 +0300

    Diana. Homework for 08_09

---
+++ src/main/java/diana/homework_8_09/Phones.java
@@ -0,0 +1,49 @@
+package diana.homework_8_09;
+
+public class Phones {
+
+    enum Smartphone {
+        iPhone_6("iPhone_6", "iPhone 6", 12.1, 4.7, 1111),
+        iPhone_X("iPhone_X", "iPhone X", 13.6, 5.8, 2222),
+        Samsung("Samsung", "Galaxy S20", 13.4, 6.2, 3333),
+        Xiaomi("Xiaomi", "Redmi Note 9", 12.6, 6.53, 4444),
+        Meizu("Meizu", "Meizu M10", 10.8, 10.8, 5555);
+
+        private String phoneName;
+        private String model;
+        private double os_version;
+        private double display;
+        private int pincode;
+
+        Smartphone(String phoneName, String model, double os_version, double display, int pincode) {
+            this.phoneName = phoneName;
+            this.model = model;
+            this.os_version = os_version;
+            this.display = display;
+            this.pincode = pincode;
+        }
+
+        public static void main(String[] args) {
+        }
+
+        public String getPhoneName() {
+            return phoneName;
+        }
+
+        public String getModel() {
+            return model;
+        }
+
+        public double getOs_version() {
+            return os_version;
+        }
+
+        public double getDisplay() {
+            return display;
+        }
+
+        public int getPincode() {
+            return pincode;
+        }
+    }
+}
